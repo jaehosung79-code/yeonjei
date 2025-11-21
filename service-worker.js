cc430ef177c0a82e71f9bb6ef5e9fa5439db17af
jaehosung79-code <jaeho.sung79@gmail.com>
2025-11-21 09:18:10 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,20 @@
-const CACHE_NAME = "exercise-app-cache-v1";
+const CACHE_NAME = "exercise-app-v1";
 const urlsToCache = [
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
-  "https://cdn.jsdelivr.net/npm/chart.js"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
-    )
-  );
-  self.clients.claim();
 });
